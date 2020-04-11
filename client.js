a3ede7ac1d0b8edacbf40b7a272e2710dae83c94
Shaokee Moua <m.shaokee@gmail.com>
2020-04-10 22:41:11 -0500

    shows in dom under seperate cells

--- client.js
+++ client.js
@@ -22,10 +22,22 @@ function submitInputs() {
     employees.push(inputsToSubmit);
     console.log(inputsToSubmit);
     displayEmployees()
-}
+}//end submitInputs
 
 function displayEmployees() {
     console.log('in displayEmployees', employees);
+    //target employee output
+    let el = $('#employeeOutputs');
+    //empty employees
+    el.empty();
+    //loop through employees
+    for (let i = 0; i < employees.length; i++) {
+        //append inputs to table
+        el.append(`<td> ${employees[i].firstname} </td>
+                   <td> ${employees[i].lastname} </td>
+                   <td> ${employees[i].idnumber} </td>
+                   <td> ${employees[i].annualsalary} </td>`)
+    }//end for loop
 
-}
+}//end displayEmployees
 
